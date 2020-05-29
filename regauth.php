b353166c509ac031b644313c51ee22d4d63f36f6
ron4mac <ron@rjconline.net>
2020-05-29 07:52:39 -0500

    update servers

--- regauth.php
+++ regauth.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * @package    Registration Authorization User Plugin
-* @copyright  (C) 2016-2019 RJCreations. All rights reserved.
+* @copyright  (C) 2016-2020 RJCreations. All rights reserved.
 * @license    GNU General Public License version 3 or later; see LICENSE.txt
 */
 defined('_JEXEC') or die;
@@ -10,7 +10,7 @@ class plgUserRegAuth extends JPlugin
 {
 	protected $autoloadLanguage = true;
 	protected $app;
-	protected $codes = array();
+	protected $codes = [];
 
 	public function __construct (&$subject, $config)
 	{
@@ -59,7 +59,7 @@ class plgUserRegAuth extends JPlugin
 	{
 		if (!$isnew || $this->app->isAdmin()) return true;
 
-		$jform = $this->app->input->post->get('jform', array(), 'array');
+		$jform = $this->app->input->post->get('jform', [], 'array');
 
 		// check for a submission (bot?) that is too quick
 		$shh = JFactory::getConfig()->get('secret');
@@ -94,7 +94,7 @@ class plgUserRegAuth extends JPlugin
 		if ($form->getName() == 'com_users.registration' && !empty($data['authcode'])) {
 			$code = trim($data['authcode']);
 			if (array_key_exists($code, $this->codes)) {
-				$data['groups'] = $this->codes[$code] ?: array(2);
+				$data['groups'] = $this->codes[$code] ?: [2];
 			}
 		}
 	}
@@ -106,15 +106,7 @@ class plgUserRegAuth extends JPlugin
 	{
 		$nonceSize = openssl_cipher_iv_length(self::METHOD);
 		$nonce = openssl_random_pseudo_bytes($nonceSize);
-
-		$ciphertext = openssl_encrypt(
-			$message,
-			self::METHOD,
-			$key,
-			OPENSSL_RAW_DATA,
-			$nonce
-		);
-
+		$ciphertext = openssl_encrypt($message, self::METHOD, $key, OPENSSL_RAW_DATA, $nonce);
 		return base64_encode($nonce.$ciphertext);
 	}
 
@@ -124,15 +116,7 @@ class plgUserRegAuth extends JPlugin
 		$nonceSize = openssl_cipher_iv_length(self::METHOD);
 		$nonce = mb_substr($message, 0, $nonceSize, '8bit');
 		$ciphertext = mb_substr($message, $nonceSize, null, '8bit');
-
-		$plaintext = openssl_decrypt(
-			$ciphertext,
-			self::METHOD,
-			$key,
-			OPENSSL_RAW_DATA,
-			$nonce
-		);
-
+		$plaintext = openssl_decrypt($ciphertext, self::METHOD, $key, OPENSSL_RAW_DATA, $nonce);
 		return $plaintext;
 	}
 
