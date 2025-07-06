c0d5853c952b0f0fc27ee2d8640066fe167cf506
ron4mac <ron@rjconline.net>
2025-07-06 11:27:06 +0000

    fully implement registration  via invitation; version 1.5.2

--- src/Extension/Regauth.php
+++ src/Extension/Regauth.php
@@ -1,9 +1,9 @@
 <?php
 /**
 * @package		plg_user_regauth
-* @copyright	Copyright (C) 2022-2024 RJCreations. All rights reserved.
+* @copyright	Copyright (C) 2022-2025 RJCreations. All rights reserved.
 * @license		GNU General Public License version 3 or later; see LICENSE.txt
-* @since		1.5.0
+* @since		1.5.2
 */
 namespace RJCreations\Plugin\User\Regauth\Extension;
 
@@ -14,10 +14,11 @@ use Joomla\CMS\Form\Form;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 
-class Regauth extends CMSPlugin
+final class Regauth extends CMSPlugin
 {
 	protected $autoloadLanguage = true;
 	protected $app;
+	protected $secret;
 	protected $codes = [];
 
 	public function __construct (&$subject, $config)
@@ -25,6 +26,8 @@ class Regauth extends CMSPlugin
 		parent::__construct($subject, $config);
 
 		if (!isset($this->app)) $this->app = Factory::getApplication();
+		// get Joomla instance secret
+		$this->secret = Factory::getConfig()->get('secret');
 		// get all auth code and group specifications
 		$authcodes = $this->params->get('authcode', []);
 		foreach ($authcodes as $ac) {
@@ -43,45 +46,39 @@ class Regauth extends CMSPlugin
 		}
 
 		if (is_object($data)) {
+			//var_dump($data);
 			// keep from using a cached time check value
 			unset($data->sbtmck);
+			// and authcode
+			unset($data->authcode);
 			// quiet complaint about array value for hidden field
 			$data->groups = 2;
+			// may be recycled
+			$astr = $data->invite??null;
 		}
 
 		$refer = $this->app->input->server->getRaw('HTTP_REFERER');
-		$astr = $this->app->input->get('_rga', '', 'base64');
-		if (($refer && strpos($refer,'registration')===false) && !$astr) $this->app->enqueueMessage($this->params->get('authnote', ''),'warning');
-
-		if ($astr) {
-			list($t, $authcode) = array_pad(explode('||', $this->orca(base64_decode($astr))),2,'');
-			if (!$authcode) {
-				$this->app->enqueueMessage(Text::_('PLG_USER_REGAUTH_INVINV'),'error');
-				return false;
-			}
-			if ($t < time()) {
-				$this->app->enqueueMessage(Text::_('PLG_USER_REGAUTH_INVEXP'),'error');
-				return false;
-			}
-		} else {
-			$authcode = false;
-		}
-
+		if (!isset($astr)) $astr = $this->app->input->get('_rga', '', 'base64');
+		$rsvp = (bool)$astr;
+		$authcode = '';
+		// see if there is a valid invitation
+		if ($rsvp) $rsvp = $this->validInvite($astr,$authcode);
 		// Add the authorization field to the form.
 		Form::addFormPath(dirname(dirname(dirname(__FILE__))).'/authform');
-
-		if ($authcode || strpos($refer,'_rga=') || ($data && isset($data->authicode))) {
+		if ($rsvp) {
 			$form->loadFile('authiform', true);
 			$form->setValue('authicode', null, $authcode);
 			$form->setValue('authcode', null, $authcode);
+			$form->setValue('invite', null, $astr);
 		} else {
 			$form->loadFile('authform', true);
 			$form->setValue('authcode', null, '');
 		}
 
+		if (($refer && strpos($refer??'','registration')===false) && !$astr) $this->app->enqueueMessage($this->params->get('authnote', ''),'warning');
+
 		// set a timecheck value to defeat rapid 'bot submissions
-		$shh = Factory::getConfig()->get('secret');
-		$form->setValue('sbtmck', null, $this->encrypt(time(), $shh));
+		$form->setValue('sbtmck', null, base64_encode($this->orca(bin2hex(time()))));
 
 		return true;
 	}
@@ -96,8 +93,8 @@ class Regauth extends CMSPlugin
 		$jform = $this->app->input->post->get('jform', [], 'array');
 
 		// check for a submission (bot?) that is too quick
-		$shh = Factory::getConfig()->get('secret');
-		$sbtm = $this->decrypt($jform['sbtmck'], $shh);
+		//$sbtm = $this->decrypt($jform['sbtmck']);
+		$sbtm = hex2bin($this->orca(base64_decode($jform['sbtmck'])));
 		if ((time() - $sbtm) < 10) {
 			throw new \Exception(Text::_('PLG_USER_REGAUTH_TOOQUICK'));
 			return false;
@@ -110,12 +107,17 @@ class Regauth extends CMSPlugin
 			return false;
 		}
 
+		// if through invitation, record the invitation useage
+		if (isset($jform['invite'])) {
+			$this->storeInvite($jform['invite']);
+		}
+
 		return true;
 	}
 
 
 	// here we can set some user default settings
-	public function onContentPrepareData ($context, $data)
+	public function onContentPrepareData ($context, $data=null)
 	{
 		if ($context == 'com_users.registration') {
 			// flag to avoid multiple message
@@ -145,30 +147,64 @@ class Regauth extends CMSPlugin
 		return array_key_exists($authcode, $this->codes);
 	}
 
-
-	const METHOD = 'aes-128-ctr';
-
-	private function encrypt ($message, $key)
+	private function storeInvite ($key)
 	{
-		$nonceSize = openssl_cipher_iv_length(self::METHOD);
-		$nonce = openssl_random_pseudo_bytes($nonceSize);
-		$ciphertext = openssl_encrypt($message, self::METHOD, $key, OPENSSL_RAW_DATA, $nonce);
-		return base64_encode($nonce.$ciphertext);
+		list($t, $code, $uses) = array_pad(explode(chr(0), $this->orca(base64_decode($key))),3,'');
+		$db = Factory::getContainer()->get('DatabaseDriver');
+		$q = $db->getQuery(true);
+		// see if it is already recorded
+		$q->select('*')->from('#__regauth_invites')->where('`key`="'.$key.'"');
+		$db->setQuery($q);
+		$row = $db->loadAssoc();
+		$q->clear();
+		if (empty($row)) {
+			// insert it
+			$q->insert('#__regauth_invites')->columns(['`key`','uses','expires'])->values(implode(',', [$db->quote($key),$uses-1,time()+86400*60]));
+			$db->setQuery($q)->execute();
+		} elseif ($row['uses']>0) {
+			// update it
+			$q->update('#__regauth_invites')->set('uses='.--$row['uses'])->where('`key`="'.$key.'"');
+			$db->setQuery($q)->execute();
+		}
+		
 	}
 
-	private function decrypt ($message, $key)
+	private function validInvite ($key, &$authcode)
 	{
-		$message = base64_decode($message);
-		$nonceSize = openssl_cipher_iv_length(self::METHOD);
-		$nonce = mb_substr($message, 0, $nonceSize, '8bit');
-		$ciphertext = mb_substr($message, $nonceSize, null, '8bit');
-		$plaintext = openssl_decrypt($ciphertext, self::METHOD, $key, OPENSSL_RAW_DATA, $nonce);
-		return $plaintext;
+		list($t, $code, $uses) = array_pad(explode(chr(0), $this->orca(base64_decode($key))),3,'');
+		if (!$uses) {
+			$this->app->enqueueMessage(Text::_('PLG_USER_REGAUTH_INVINV'),'error');
+			return false;
+		}
+		if ($t < time()) {
+			$this->app->enqueueMessage(Text::_('PLG_USER_REGAUTH_INVEXP'),'error');
+			return false;
+		}
+
+		$db = Factory::getContainer()->get('DatabaseDriver');
+		// clear historically expired items
+		$q = $db->getQuery(true)
+			->delete('#__regauth_invites')
+			->where('expires<'.time());
+		$db->setQuery($q)->execute();
+		$q->clear();
+		$q->select('*')
+			->from('#__regauth_invites')
+			->where('`key` = "'.$key.'"');
+		$db->setQuery($q);
+		$row = $db->loadAssoc();
+		if (empty($row) || $row['uses']>0) {
+			$authcode = $code;
+			return true;
+		}
+		$this->app->enqueueMessage(Text::_('PLG_USER_REGAUTH_INVINV'),'error');
+		return false;
 	}
 
+	// simple but sufficiently effective XOR encrypt/decrypt
 	private function orca ($p)
 	{
-		$q = Factory::getConfig()->get('secret');
+		$q = $this->secret;
 		$l = strlen($q);
 		$r = '';
 		while ($p) {
